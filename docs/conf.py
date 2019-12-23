190329a05d739f410182df6b9e693cd2108c1969
Vinayaka Jyothi <vinayaka.jyothi>
2019-12-23 19:55:45 +0530

    Updated Theme

--- docs/conf.py
+++ docs/conf.py
@@ -64,4 +64,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_theme = "alabaster"
+import sphinx_rtd_theme
+
+extensions = [
+    ...
+    "sphinx_rtd_theme",
+]
+
+html_theme = "sphinx_rtd_theme"
