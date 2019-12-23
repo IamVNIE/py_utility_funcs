1be53f3a5a34458f2fe0e5dd4cefbce5e34cbd00
Vinayaka Jyothi <vinayaka.jyothi>
2019-12-23 19:46:41 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -62,6 +62,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-import stanford_theme
-html_theme = "stanford_theme"
-html_theme_path = [stanford_theme.get_html_theme_path()]
+html_theme = "master"
